513358ed8850d8d44ae750398aee818d7af1fda8
Tolemak <60557277+Tolemak@users.noreply.github.com>
2024-05-12 21:27:40 +0200

    testing some commands

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import "traverian";
 
-const app = traverian();
+// const app = traverian();
+
+import { decodeHTML } from 'entities';
+
+// Assuming you have a string with HTML entities
+const htmlEntitiesString = 'Hello &amp; welcome!';
+
+// Decode the HTML entities
+const decodedString = decodeHTML(htmlEntitiesString);
+
+console.log(decodedString); // Output: Hello & welcome!
